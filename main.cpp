3457113f7a756a6a173d3902fa98212300d22b8d
Victor Ignatenkov <vetekm@yahoo.com>
2015-12-04 20:22:05 -0800

    Add output and get rid of cout.
    Algorithms and average calculations work.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-//TODO: Don't forget to add AVG Waiting Time: ______
 
 typedef enum {
     SJF,
@@ -23,8 +22,11 @@ typedef enum {
 };
 void runSjfScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process);
 void runRRScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process, const int timeQuantam);
-
 void runPRNoPreempScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process);
+void printAlgorithmName(int algType);
+
+ofstream outf("output.txt");
+
 
 int main() {
 
@@ -33,6 +35,15 @@ int main() {
     int maxNumOfProcesses;
     struct processInfo *process;
     int const timeQuantam = 4;
+    int avg_time = 0;
+
+
+
+      if(!outf){
+          cerr << "Uh oh, output.txt could not be opened for writing!" << endl;
+          cout << "error";
+          exit(1);
+      }
 
 
     ifstream inf("input.txt");
@@ -44,6 +55,7 @@ int main() {
     while( getline(inf, strInput)){
 
         stringstream strStream;
+
         if(strInput.compare("SJF") == 0){
             algType = ::SJF;
         } else if (strInput.compare("RR") == 0){
@@ -56,7 +68,7 @@ int main() {
 
 
 
-        cout << "Algorithm type is: " << algType << endl;
+
         getline(inf, strInput);
         strStream << strInput;
         strStream >> maxNumOfProcesses;
@@ -94,6 +106,7 @@ int main() {
             cout << endl;
          }
 
+        printAlgorithmName(algType);
         if(algType == ::SJF){
         runSjfScheduler(numOfProcesses, maxNumOfProcesses, process);}
         else if(algType == ::RR) {
@@ -102,9 +115,20 @@ int main() {
             runPRNoPreempScheduler(numOfProcesses, maxNumOfProcesses, process);
         } else {
 
+
             numOfProcesses = 0;
             int curTime = 0;
-            cout << "Max number of processes is: " << maxNumOfProcesses << endl;
+            int total_time = 0;
+            int mum_of_iter = 0;
+
+            int prev_process = 0;
+            int current_process;
+            int *process_wait_time = new int[maxNumOfProcesses+2];
+            for(int i = 0; i < maxNumOfProcesses+2; i++){
+                process_wait_time[i]  = 0;
+            }
+
+           // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
             priority_queue<processInfo*,vector<processInfo*>,function
                     <bool(const processInfo*,const processInfo*)>>
                     prQueuePR([](const processInfo* s1, const processInfo* s2)
@@ -112,29 +136,64 @@ int main() {
 //TODO: When does it get preempted, every time or only if a next process' priority is higher?
             while(numOfProcesses < maxNumOfProcesses){
                 prQueuePR.push((processInfo*)(&process[numOfProcesses]));
+                current_process = ((processInfo*)prQueuePR.top())->number;
                 if(numOfProcesses != maxNumOfProcesses - 1) {    // Don't compare the last element
                     processInfo *info = prQueuePR.top();
 
                     int timeOffset = curTime + info->cpuBurst;
                     if(timeOffset < process[numOfProcesses+1].timeArrive){ // <= ???
-                        cout << curTime << " " << info->number << endl;
-                        curTime += info->cpuBurst;
+                        //cout << curTime << " " << info->number << endl;
+                        outf << "\t" << curTime << " " << info->number << endl;
+
+                        if(prev_process != current_process) {
+                            total_time += curTime - process_wait_time[current_process];
+                        }
+
+                        curTime += info->cpuBurst; //total_time += curTime;
                         prQueuePR.pop();
+                        mum_of_iter++;
+
+                        if(prev_process != current_process) {
+                            process_wait_time[current_process] =  curTime;
+                            prev_process = current_process;
+                        }
+
                     }else {
                         processInfo *info = prQueuePR.top();
                         info->cpuBurst -= process[numOfProcesses + 1].timeArrive - curTime;
-                        cout << curTime << " " << info->number << endl;
+                        //cout << curTime << " " << info->number << endl;
+                        outf << "\t" << curTime << " " << info->number << endl;
+
                         curTime += process[numOfProcesses + 1].timeArrive - curTime;
+                        mum_of_iter++;
+
                     }
                 }
                 numOfProcesses++;
             }
             while(!prQueuePR.empty()){
                 processInfo *info = prQueuePR.top();    prQueuePR.pop();
-                cout << curTime << " " << info->number << endl;
+                //cout << curTime << " " << info->number << endl;
+                outf << "\t" << curTime << " " << info->number << endl;
+                if(prev_process != current_process) {
+                    total_time += curTime - process_wait_time[current_process];
+                }
+
                 curTime += info->cpuBurst;
+                mum_of_iter++;
+
+                if(prev_process != current_process) {
+                    process_wait_time[current_process] =  curTime;
+                    prev_process = current_process;
+                }
             }
+
+            //cout << "AVG Waiting Time: " << (float)total_time / maxNumOfProcesses << endl << endl << endl;
+            outf << "AVG Waiting Time: " << (float)total_time / maxNumOfProcesses << endl << endl << endl;
         }
+
+
+
         delete [] process;
     }
 
@@ -143,63 +202,34 @@ int main() {
 
 
 
-    /**
-     * File manipulation
 
-    ofstream outf("output.txt");
-    if(!outf){
-        cerr << "Uh oh, output.txt could not be opened for writing!" << endl;
-        cout << "error";
-        exit(1);
-    }
 
-    // Write two lines into the file
-    outf << "This is line 1" << endl;
-    outf << "This is line 2" << endl;
 
-  */
-    /**
+    /*
      * ifstream returns a 0 if we’ve reached the end of the file (EOF)
      */
-    /*
-
-
-  -------------4----------------
-  PR_withPREMP
-  4
-  1     0      8       3
-  2     3      1       1
-
-  3     5      2       4
-  4     6      2       2
-
-  curTime = 0;
-  1     0      8       3
-  Give only 3 seconds to the first process by looking in the time queue
-  Add the second process after three seconds to the main queue and change
-  the first process
-
-  curTime = 3;
-  2     3      1       1
-
-  curTime + 1 = 4;
-
-  curTime = 4;
-  1     0      5       3
-  4 + 5 = 9
-  5 - 4 = 1;
-  curTime += 1
-
-*/
 
 
     return 0;
 }
 
+void printAlgorithmName(int algType) {
+             if(algType == SJF){
+                outf << "SJF" << endl;
+            } else if (algType == RR){
+                outf << "RR" << endl;
+            } else if (algType == PR_noPREMP){
+                outf << "PR_noPREMP" << endl;
+            } else if (algType == PR_withPREMP){
+                outf << "PR_withPREMP" << endl;
+            }
+}
+
 void runPRNoPreempScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process) {
     numOfProcesses = 0;
     int curTime = 0;
-    cout << "Max number of processes is: " << maxNumOfProcesses << endl;
+    int total_time = 0;
+   // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
     priority_queue<processInfo*,vector<processInfo*>,function
                     <bool(const processInfo*,const processInfo*)>>
                     prQueuePR([](const processInfo* s1, const processInfo* s2)
@@ -211,7 +241,8 @@ void runPRNoPreempScheduler(int numOfProcesses, int maxNumOfProcesses, const pro
                     if (process[numOfProcesses + 1].timeArrive > curTime) {
                         processInfo *info = prQueuePR.top();
                         prQueuePR.pop();
-                        cout << curTime << " " << info->number << endl;
+                        //cout << curTime << " " << info->number << endl; total_time += curTime;
+                        outf << "\t" << curTime << " " << info->number << endl;
                         curTime += info->cpuBurst;
                     }
                 }
@@ -219,15 +250,28 @@ void runPRNoPreempScheduler(int numOfProcesses, int maxNumOfProcesses, const pro
             }
     while(!prQueuePR.empty()){
                 processInfo *info = prQueuePR.top();    prQueuePR.pop();
-                cout << curTime << " " << info->number << endl;
+                //cout << curTime << " " << info->number << endl;  total_time += curTime;
+                outf << "\t"   << curTime << " " << info->number << endl;
                 curTime += info->cpuBurst;
             }
+    //cout << "AVG Waiting Time: " << (float)total_time /  numOfProcesses << endl << endl << endl;
+    outf << "AVG Waiting Time: " << (float)total_time /  numOfProcesses << endl << endl << endl;
 }
 
 void runRRScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process, const int timeQuantam) {
     numOfProcesses = 0;
     int curTime = 0;
-    cout << "Max number of processes is: " << maxNumOfProcesses << endl;
+    int total_time = 0;
+    int num_of_iter = 0;
+    int prev_process = 0;
+    int current_process;
+    int *process_wait_time = new int[maxNumOfProcesses+2];
+    for(int i = 0; i < maxNumOfProcesses+2; i++){
+        process_wait_time[i]  = 0;
+    }
+
+
+   // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
     queue<processInfo*> regQueueRR;
     while(numOfProcesses < maxNumOfProcesses){
                 regQueueRR.push((processInfo*)(&process[numOfProcesses]));
@@ -235,28 +279,70 @@ void runRRScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo
             }
     while(!regQueueRR.empty()){
                 int processBurst = ((processInfo*)regQueueRR.front())->cpuBurst;
+                current_process = ((processInfo*)regQueueRR.front())->number;
+
                 if(processBurst > timeQuantam){
                     //do a swap
                     processInfo *info = regQueueRR.front();
                     info->cpuBurst -= timeQuantam;
-                    cout << curTime << " " << info->number << endl;
+                    //cout << curTime << " " << info->number << endl;
+                    outf << "\t"  << curTime << " " << info->number << endl;
+                    //total_time += curTime;
+
+                    if(prev_process != current_process) {
+                        total_time += curTime - process_wait_time[current_process];
+                        num_of_iter++;
+                    }
+
                     regQueueRR.push(info);
                     regQueueRR.pop();
                     curTime += timeQuantam;
+
+                    if(prev_process != current_process) {
+                        process_wait_time[current_process] =  curTime;
+                        num_of_iter++;
+                        prev_process = current_process;
+                    }
                 } else {
                     processInfo *info = regQueueRR.front();
-                    cout << curTime << " " << info->number << endl;
+                    //cout << curTime << " " << info->number << endl;
+                    outf << "\t"  << curTime << " " << info->number << endl;
+
+                    if(prev_process != current_process) {
+                        total_time += curTime - process_wait_time[current_process];
+                        num_of_iter++;
+
+                    }
+
                     curTime += info->cpuBurst;
                     regQueueRR.pop();
 
+                    if(prev_process != current_process) {
+                        process_wait_time[current_process] =  curTime;
+                        num_of_iter++;
+                        prev_process = current_process;
+                    }
+
+
+
                 }
             }
+
+    for(int i = 0; i < numOfProcesses; i++){
+       // total_time += process_wait_time[i];
+    }
+
+    //cout << "AVG Waiting Time: " << (float)total_time / maxNumOfProcesses << endl << endl << endl;
+    outf << "AVG Waiting Time: " << (float)total_time / maxNumOfProcesses << endl << endl << endl;
 }
 
 void runSjfScheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process) {
     numOfProcesses = 0;
     int curTime = 0;
-    cout << "Max number of processes is: " << maxNumOfProcesses << endl;
+    int total_time = 0;
+
+
+   // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
     priority_queue<processInfo*,vector<processInfo*>,function
                 <bool(const processInfo*,const processInfo*)>>
                 prQueueSJF([](const processInfo* s1, const processInfo* s2)
@@ -269,15 +355,26 @@ void runSjfScheduler(int numOfProcesses, int maxNumOfProcesses, const processInf
                 if (process[numOfProcesses + 1].timeArrive > curTime) {
                     processInfo *info = prQueueSJF.top();
                     prQueueSJF.pop();
-                    cout << curTime << " " << info->number << endl;
+                    //cout << curTime << " " << info->number << endl;
+                    outf << "\t"   << curTime << " " << info->number << endl;
                     curTime += info->cpuBurst;
+
                 }
             }
             numOfProcesses++;
         }
     while(!prQueueSJF.empty()){
             processInfo *info = prQueueSJF.top();    prQueueSJF.pop();
-            cout << curTime << " " << info->number << endl;
+            //cout << curTime << " " << info->number << endl;  total_time += curTime;
+            outf << "\t"  << curTime << " " << info->number << endl;
             curTime += info->cpuBurst;
+
         }
+
+    //cout << "AVG Waiting Time: " << (float)total_time /  numOfProcesses << endl << endl << endl;
+    outf << "AVG Waiting Time: " << (float)total_time /  numOfProcesses << endl << endl << endl;
+
+
 }
+
+
