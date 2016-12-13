efb9b3d34665803357e42f43d2e4794b4e527c27
Victor Ignatenkov <vetekm@yahoo.com>
2016-12-13 14:12:41 -0800

    Refactored and fixed Priority Scheduling with Preemption

--- main.cpp
+++ main.cpp
@@ -23,10 +23,16 @@ struct processInfo {
 };
 
 void run_SJF_scheduler(int seq_num_of_curr_process, int num_of_processes, const processInfo *process);
-void run_RR_scheduler(int num_of_processes, int max_num_of_processes, const processInfo *process, const int time_quantam);
-void run_PR_NO_PREEM_scheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process);
+
+void
+run_RR_scheduler(int num_of_processes, int max_num_of_processes, const processInfo *process, const int time_quantam);
+
+void run_PR_NO_PREEM_scheduler(int seq_num_of_curr_process, int num_of_processes, const processInfo *process);
+
 void printAlgorithmName(int algType);
-int run_PR_WITH_PREEM_schedulingAlg(int numOfProcesses, int maxNumOfProcesses, const processInfo *process);
+
+void run_PR_WITH_PREEM_schedulingAlg(int seq_num_of_curr_process, int num_of_processes, const processInfo *process);
+
 ofstream outf("output.txt");
 
 int main() {
@@ -127,91 +133,10 @@ int main() {
         } else if (ALG_TYPE[i] == ::PR_NO_PREEMP) {
             run_PR_NO_PREEM_scheduler(numOfProcesses, maxNumOfProcesses, copyOfTheOriginalProcesses);
         } else {
-            numOfProcesses = run_PR_WITH_PREEM_schedulingAlg(numOfProcesses, maxNumOfProcesses, process);
+            run_PR_WITH_PREEM_schedulingAlg(numOfProcesses, maxNumOfProcesses, process);
         }
     }
-
-
     return 0;
-}
-
-int run_PR_WITH_PREEM_schedulingAlg(int numOfProcesses, int maxNumOfProcesses, const processInfo *process) {
-    numOfProcesses = 0;
-    int curTime = 0;
-    int total_time = 0;
-    int mum_of_iter = 0;
-
-    int prev_process = 0;
-    int current_process;
-    int *process_wait_time = new int[maxNumOfProcesses + 2];
-    for (int i = 0; i < maxNumOfProcesses + 2; i++) {
-        process_wait_time[i] = 0;
-    }
-
-    // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
-    priority_queue<processInfo *, vector<processInfo *>, function
-            <bool(const processInfo *, const processInfo *)>>
-            prQueuePR([](const processInfo *s1, const processInfo *s2) { return s1->priority > s2->priority; });
-//TODO: When does it get preempted, every time or only if a next process' priority is higher?
-    while (numOfProcesses < maxNumOfProcesses) {
-        prQueuePR.push((processInfo *) (&process[numOfProcesses]));
-        current_process = ((processInfo *) prQueuePR.top())->number;
-        if (numOfProcesses != maxNumOfProcesses - 1) {    // Don't compare the last element
-            processInfo *info = prQueuePR.top();
-
-            int timeOffset = curTime + info->cpuBurst;
-            if (timeOffset < process[numOfProcesses + 1].timeArrive) { // <= ???
-                //cout << curTime << " " << info->number << endl;
-                outf << "\t" << curTime << " " << info->number << endl;
-
-                if (prev_process != current_process) {
-                    total_time += curTime - process_wait_time[current_process];
-                }
-
-                curTime += info->cpuBurst; //total_time += curTime;
-                prQueuePR.pop();
-                mum_of_iter++;
-
-
-                if (prev_process != current_process) {
-                    process_wait_time[current_process] = curTime;
-                    prev_process = current_process;
-                }
-
-            } else {
-                processInfo *info = prQueuePR.top();
-                info->cpuBurst -= process[numOfProcesses + 1].timeArrive - curTime;
-                //cout << curTime << " " << info->number << endl;
-                outf << "\t" << curTime << " " << info->number << endl;
-
-                curTime += process[numOfProcesses + 1].timeArrive - curTime;
-                mum_of_iter++;
-
-            }
-        }
-        numOfProcesses++;
-    }
-    while (!prQueuePR.empty()) {
-        processInfo *info = prQueuePR.top();
-        prQueuePR.pop();
-        //cout << curTime << " " << info->number << endl;
-        outf << "\t" << curTime << " " << info->number << endl;
-        if (prev_process != current_process) {
-            total_time += curTime - process_wait_time[current_process];
-        }
-
-        curTime += info->cpuBurst;
-        mum_of_iter++;
-
-        if (prev_process != current_process) {
-            process_wait_time[current_process] = curTime;
-            prev_process = current_process;
-        }
-    }
-
-    //cout << "AVG Waiting Time: " << (float)total_time / maxNumOfProcesses << endl << endl << endl;
-    outf << "AVG Waiting Time: " << (float) total_time / maxNumOfProcesses << endl << endl << endl;
-    return numOfProcesses;
 }
 
 void printAlgorithmName(int algType) {
@@ -224,52 +149,127 @@ void printAlgorithmName(int algType) {
         outf << "*       Round Robin       * " << endl;
         outf << "* * * * * * * * * * * * * *" << endl << endl;
     } else if (algType == PR_NO_PREEMP) {
-        outf << "PR_NO_PREEMP" << endl;
+        outf << "* * * * * * * * * * * * * * * * " << endl;
+        outf << "* Priority without preemption * " << endl;
+        outf << "* * * * * * * * * * * * * * * * " << endl << endl;
     } else if (algType == PR_WITH_PREEMP) {
         outf << "PR_WITH_PREEMP" << endl;
     }
 }
 
-void run_PR_NO_PREEM_scheduler(int numOfProcesses, int maxNumOfProcesses, const processInfo *process) {
-    numOfProcesses = 0;
-    int curTime = 0;
-    int total_time = 0;
-    // cout << "Max number of processes is: " << maxNumOfProcesses << endl;
+
+void run_PR_WITH_PREEM_schedulingAlg(int seq_num_of_curr_process, int num_of_processes, const processInfo *process) {
+    int TIME_QUANTAM = 1;
+    seq_num_of_curr_process = 0;
+    int seq_num_of_last_process = -1;
+    int cur_time = 0;
+    int total_waiting_time = 0;
+
+    int *time_it_finished_last_time = new int[num_of_processes + 2];
+    for (int i = 0; i < num_of_processes + 2; i++) {
+        time_it_finished_last_time[i] = process[i].timeArrive;
+    }
+
     priority_queue<processInfo *, vector<processInfo *>, function
             <bool(const processInfo *, const processInfo *)>>
-            prQueuePR([](const processInfo *s1, const processInfo *s2) { return s1->priority > s2->priority; });
+            prior_queue_of_curr_processes(
+            [](const processInfo *s1, const processInfo *s2) { return s1->priority > s2->priority; });
 
-    while (numOfProcesses < maxNumOfProcesses) {
-        prQueuePR.push((processInfo *) (&process[numOfProcesses]));
-        if (numOfProcesses != maxNumOfProcesses - 1) {    // Don't compare the last element
-            if (process[numOfProcesses + 1].timeArrive > curTime) {
-                processInfo *info = prQueuePR.top();
-                prQueuePR.pop();
-                //cout << curTime << " " << info->number << endl;
-                total_time += curTime;
-                outf << "\t" << curTime << " " << info->number << endl;
-                curTime += info->cpuBurst;
+    list<processInfo *> all_processes;
+    while (seq_num_of_curr_process < num_of_processes) {
+        all_processes.push_back((processInfo *) (&process[seq_num_of_curr_process]));
+        seq_num_of_curr_process++;
+    }
+
+    list<processInfo *>::iterator iter;
+    while (!all_processes.empty()) {
+        for (iter = all_processes.begin(); iter != all_processes.end(); ++iter) {
+            if ((*iter)->timeArrive <= cur_time) {
+                prior_queue_of_curr_processes.push(*iter);
+                all_processes.erase(iter);
             }
         }
-        numOfProcesses++;
+
+        processInfo *curr_proc = prior_queue_of_curr_processes.top();
+        seq_num_of_curr_process = curr_proc->number;
+        if (seq_num_of_curr_process != seq_num_of_last_process) {
+            total_waiting_time = cur_time - time_it_finished_last_time[seq_num_of_last_process];
+            outf << "\t" << curr_proc->number << " : " << cur_time << endl;
+        }
+
+        curr_proc->cpuBurst -= TIME_QUANTAM;
+
+        if (curr_proc->cpuBurst == 0) {
+            prior_queue_of_curr_processes.pop();
+        }
+        seq_num_of_last_process = seq_num_of_curr_process;
+        time_it_finished_last_time[seq_num_of_curr_process] = cur_time;
+        cur_time++;
     }
-    while (!prQueuePR.empty()) {
-        processInfo *info = prQueuePR.top();
-        prQueuePR.pop();
-        //cout << curTime << " " << info->number << endl;
-        total_time += curTime;
-        outf << "\t" << curTime << " " << info->number << endl;
-        curTime += info->cpuBurst;
+
+    while (!prior_queue_of_curr_processes.empty()) {
+        processInfo *cur_process = prior_queue_of_curr_processes.top();
+        seq_num_of_curr_process = cur_process->number;
+        prior_queue_of_curr_processes.pop();
+        if (seq_num_of_last_process != seq_num_of_curr_process) {
+            outf << "\t" << cur_process->number << " : " << cur_time << endl;
+            total_waiting_time += cur_time - time_it_finished_last_time[seq_num_of_curr_process];
+        }
+        cur_time += cur_process->cpuBurst;
+        seq_num_of_last_process = seq_num_of_curr_process;
     }
-    //cout << "AVG Waiting Time: " << (float)total_time /  numOfProcesses << endl << endl << endl;
-    outf << "AVG Waiting Time: " << (float) total_time / numOfProcesses << endl << endl << endl;
+    outf << "\nAverage waiting time: " << (float) total_waiting_time / num_of_processes << endl << endl << endl;
+}
+
+
+// Process number           Arrival Time           CPU burst time            Priority
+//4
+//1     0      8       3
+//2     3      1       1
+//3     5      2       4
+//4     6      2       2
+void run_PR_NO_PREEM_scheduler(int seq_num_of_curr_process, int num_of_processes, const processInfo *process) {
+    seq_num_of_curr_process = 0;
+    int cur_time = 0;
+    int total_waiting_time = 0;
+
+    priority_queue<processInfo *, vector<processInfo *>, function
+            <bool(const processInfo *, const processInfo *)>>
+            prior_queue_of_curr_processes(
+            [](const processInfo *s1, const processInfo *s2) { return s1->priority > s2->priority; });
+
+    list<processInfo *> all_processes;
+    while (seq_num_of_curr_process < num_of_processes) {
+        all_processes.push_back((processInfo *) (&process[seq_num_of_curr_process]));
+        seq_num_of_curr_process++;
+    }
+
+    list<processInfo *>::iterator iter;
+    while (!all_processes.empty()) {
+        for (iter = all_processes.begin(); iter != all_processes.end(); ++iter) {
+            if ((*iter)->timeArrive <= cur_time) {
+                prior_queue_of_curr_processes.push(*iter);
+                all_processes.erase(iter);
+            }
+        }
+        while (!prior_queue_of_curr_processes.empty()) {
+            processInfo *cur_process = prior_queue_of_curr_processes.top();
+            prior_queue_of_curr_processes.pop();
+            outf << "\t" << cur_process->number << " : " << cur_time << endl;
+            total_waiting_time += cur_time;
+            cur_time += cur_process->cpuBurst;
+        }
+    }
+
+    outf << "\nAverage waiting time: " << (float) total_waiting_time / seq_num_of_curr_process << endl << endl << endl;
 }
 
 // Process number           Arrival Time           CPU burst time            Priority
-void run_RR_scheduler(int num_of_processes, int max_num_of_processes, const processInfo *process, const int time_quantam) {
+void
+run_RR_scheduler(int num_of_processes, int max_num_of_processes, const processInfo *process, const int time_quantam) {
     num_of_processes = 0;
     int curr_time = 0;
-    int total_time_waiting_time = 0;
+    int total_waiting_time = 0;
     int prev_process_seq_number = 0;
     int curr_process_seq_number;
     int *time_it_finished_last_time = new int[max_num_of_processes + 2];
@@ -289,10 +289,10 @@ void run_RR_scheduler(int num_of_processes, int max_num_of_processes, const proc
         if (curr_process_BURST > time_quantam) {
             processInfo *curr_proc = rr_queue.front();
             curr_proc->cpuBurst -= time_quantam;
-            outf << "\t" << curr_proc->number  << " : " <<  curr_time<< endl;
+            outf << "\t" << curr_proc->number << " : " << curr_time << endl;
 
-            if (prev_process_seq_number!= curr_process_seq_number) {
-                total_time_waiting_time += curr_time - time_it_finished_last_time[curr_process_seq_number];
+            if (prev_process_seq_number != curr_process_seq_number) {
+                total_waiting_time += curr_time - time_it_finished_last_time[curr_process_seq_number];
             }
 
             rr_queue.push(curr_proc);
@@ -305,10 +305,10 @@ void run_RR_scheduler(int num_of_processes, int max_num_of_processes, const proc
             }
         } else { //If Current Process' Burst is LESS OR EQUAL to the TIME QUANTUM
             processInfo *curr_proc = rr_queue.front();
-            outf << "\t" << curr_proc->number << " : " <<  curr_time<< endl;
+            outf << "\t" << curr_proc->number << " : " << curr_time << endl;
 
             if (prev_process_seq_number != curr_process_seq_number) {
-                total_time_waiting_time += curr_time - time_it_finished_last_time[curr_process_seq_number];
+                total_waiting_time += curr_time - time_it_finished_last_time[curr_process_seq_number];
             }
 
             curr_time += curr_proc->cpuBurst;
@@ -321,7 +321,8 @@ void run_RR_scheduler(int num_of_processes, int max_num_of_processes, const proc
         }
     }
 
-    outf << endl <<  "Average waiting time: " << (float) total_time_waiting_time / max_num_of_processes << endl << endl << endl;
+    outf << endl << "Average waiting time: " << (float) total_waiting_time / max_num_of_processes << endl << endl
+         << endl;
 }
 
 // Process number           Arrival Time           CPU burst time            Priority
@@ -333,29 +334,30 @@ void run_SJF_scheduler(int seq_num_of_curr_process, int num_of_processes, const 
     priority_queue<processInfo *, //Define an element
             vector<processInfo *>, //For Internal Use
             function<bool(const processInfo *, const processInfo *)>> //Comparator
-            prior_queue_curr_processes([](const processInfo *s1, const processInfo *s2) { return s1->cpuBurst > s2->cpuBurst; });
+            prior_queue_curr_processes(
+            [](const processInfo *s1, const processInfo *s2) { return s1->cpuBurst > s2->cpuBurst; });
 
     //Add all processes to a list
-    list<processInfo *> all_processes;
+    list<processInfo *> all_processes; //TODO: isn't better to use a vector ???
     while (seq_num_of_curr_process < num_of_processes) {
-        all_processes.push_back((processInfo*)(&process[seq_num_of_curr_process]));
+        all_processes.push_back((processInfo *) (&process[seq_num_of_curr_process]));
         seq_num_of_curr_process++;
     }
 
-    list<processInfo*>::iterator iter;
-    while(!all_processes.empty()){
-        for(iter = all_processes.begin(); iter != all_processes.end(); ++iter){
-            if((*iter)->timeArrive <= curr_time){
+    list<processInfo *>::iterator iter;
+    while (!all_processes.empty()) {
+        for (iter = all_processes.begin(); iter != all_processes.end(); ++iter) {
+            if ((*iter)->timeArrive <= curr_time) {
                 prior_queue_curr_processes.push(*iter);
                 all_processes.erase(iter);
             }
         }
-        while(!prior_queue_curr_processes.empty()){
-            processInfo *info = prior_queue_curr_processes.top();
+        while (!prior_queue_curr_processes.empty()) {
+            processInfo *curr_proc = prior_queue_curr_processes.top();
             prior_queue_curr_processes.pop();
-            outf << "\t" << info->number << " : " << curr_time << endl;
+            outf << "\t" << curr_proc->number << " : " << curr_time << endl;
             total_time_waiting_time += curr_time;
-            curr_time += info->cpuBurst;
+            curr_time += curr_proc->cpuBurst;
         }
     }
     outf << "\nAverage waiting time: " << (float) total_time_waiting_time / num_of_processes << endl << endl << endl;
